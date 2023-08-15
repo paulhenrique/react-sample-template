d95b13d33cb66ef787b41e5aba702d185f2940f8
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-15 16:49:22 -0300

    Adiciona estrutura base

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/css/normalize.css";
+import "./assets/css/global.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
